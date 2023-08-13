8483c81d6ab5ffe9665bbd554050be928f2ced78
Md. Minhazul Haque <ashim.ashim7470@gmail.com>
2023-08-13 21:05:19 +0600

    shortest distance algorithm implemented for unweighted graphs

--- module3/bfsForShortestDistance.cpp
+++ module3/bfsForShortestDistance.cpp
@@ -5,28 +5,30 @@ void init_code() {
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    #endif
+    #endif //ONLINE JUDGE
 }
 
 const int N = 1e5 + 5;
 vector<int> adj[N];
-int visited[N];
 int level[N];
+int visited[N];
+int parent[N];
 
 void bfs(int s) {
     queue<int> q;
     q.push(s);
     visited[s] = true;
     level[s] = 0;
+    parent[s] = -1;
     while(!q.empty()) {
         int u = q.front();
         q.pop();
-        cout << "Visiting Node " << u << endl;
         for(int v : adj[u]) {
             if(visited[v]) continue;
             q.push(v);
             visited[v] = true;
             level[v] = level[u] + 1;
+            parent[v] = u;
         }
     }
 }
@@ -41,13 +43,25 @@ int main () {
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
+    int s, d;
+    cin >> s >> d;
+    bfs(s);
+    cout << "Shortest distance from " << s << " to " << d << " is " << level[d] << endl;
+    // for (int i = 1; i <= n; i++) {
+    //     cout << "Parent of " << i << " is " << parent[i] << endl;
+    // }
+    vector<int> path;
+    int current = d;
 
-    bfs(1);
+    while(parent[current] != -1) {
+        path.push_back(current);
+        current = parent[current];
+    }
 
-    cout << endl;
-
-    for (int i = 1; i <= n; i++) {
-        cout << "Level of " << i << " is " << level[i] << endl;
+    reverse(path.begin(), path.end());
+    cout << "Shortest Path from " << s << " to " << d << " is ";
+    for(int x : path) {
+        cout << x << " ";
     }
     return 0;
 }
