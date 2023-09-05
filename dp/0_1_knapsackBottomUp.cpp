0d2d9772bfc8988241e83258a6b68c20e9788a1d
Minhazul Ashim <ashim.ashim7470@gmail.com>
2023-09-05 20:19:54 +0600

    corrected knapsack bottom up code

--- dp/0_1_knapsackBottomUp.cpp
+++ dp/0_1_knapsackBottomUp.cpp
@@ -42,8 +42,8 @@ int main () {
 
     int maximum = 0;
 
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j < s; j++) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= s; j++) {
             if(w[i-1] <= j) {
                 int op1 = dp[i - 1][j - w[i - 1]] + v[i - 1];
                 int op2 = dp[i - 1][j];
@@ -57,7 +57,13 @@ int main () {
         }
     }
 
-    cout << maximum;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= s; j++) {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
+
 
     return 0;
 }
