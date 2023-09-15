d25c4664fd4ceeab8107eeb6a68a744f1f67f40a
Minhazul Ashim <ashim.ashim7470@gmail.com>
2023-09-15 13:20:04 +0600

    solved all the problems of algorithm final exam

---
+++ algoFinal/makePalindrome.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void init_code() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main () {
+    init_code();
+    int t;
+    cin >> t;
+    while(t--) {
+        string a;
+        cin >> a;
+        int n = a.size();
+        string b = a;
+        reverse(b.begin(), b.end());
+        int m = b.size();
+        int dp[n + 1][m + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= m; j++)
+            {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+            }
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (a[i - 1] == b[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        int l = dp[n][m];
+        int count = n - l;
+        cout << count << endl;
+    }
+    return 0;
+}
